78c7793fcdab7c0400cd2eacae64afb8adaca29a
Jake Robinson <jake.robinson11@gmail.com>
2024-08-01 18:13:37 +0200

    8kyu hello world complete

---
+++ 8kyu-hello-world/js/main.js
@@ -0,0 +1,7 @@
+//You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+class Solution{
+    static main() {
+      console.log("Hello World!");
+    }  
+  }
