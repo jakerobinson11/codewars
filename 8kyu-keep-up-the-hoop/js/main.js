1f2fc8800a5817ccb09d5508bb3c3443fe97b533
Jake Robinson <jake.robinson11@gmail.com>
2024-08-01 18:20:10 +0200

    Completed 8kyu keep up the hoop

---
+++ 8kyu-keep-up-the-hoop/js/main.js
@@ -0,0 +1,10 @@
+//Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message:
+
+function hoopCount (n) {
+  //your code goes here  
+ if (n >= 10){
+   return "Great, now move on to tricks"
+ } else {
+   return "Keep at it until you get it"
+ }
+}
